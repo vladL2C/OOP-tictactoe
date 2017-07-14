2cf5bbd34101d73450e52cc70834bd40b9f49fd9
Vlad <vlad@hotmail.co.nz>
2017-07-13 21:51:47 -0700

    finished win

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -27,6 +27,15 @@ describe 'Board' do
 			it 'marks the board where player moves' do 
 				expect(board.player_move(1,"x")).to eql(["x",2,3,4,5,6,7,8,9]) 
 			end 
-		end 	
-	
+		end 
+
+	describe '#win?' do 
+		let(:player) {"vlad"}
+		it 'lets you know what user has lost' do 
+			      board.board = ["X","X","X",
+                     				4,5,6,
+                     				7,8,9]
+		expect(board.win?(player)).to eql("Better luck next time! #{player.capitalize}")		
+		end
+	end  
 end 
